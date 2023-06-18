589be12611475031fa2c9a9c5c19fc80121d46d3
pedrorodrigues1997 <45696053+pedrorodrigues1997@users.noreply.github.com>
2023-06-18 21:24:57 +0100

    User do not need to be logged in to see other user

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,7 +4,7 @@ import { verifyToken } from "../middleware/jwt.js";
 const router = express.Router();
 
 router.delete("/:id", verifyToken,deleteUser);
-router.get("/:id", verifyToken,getUser);
+router.get("/:id",getUser);
 
 export default router;
 
